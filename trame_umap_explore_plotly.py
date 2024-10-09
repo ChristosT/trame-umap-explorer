ad838562068058509cfdf06b98e75d0d7fad72e6
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-10-09 18:31:22 -0400

    Add pca method for dimensionality reduction

--- trame_umap_explore_plotly.py
+++ trame_umap_explore_plotly.py
@@ -11,9 +11,11 @@ from trame.widgets import vuetify3, plotly, html
 import numpy as np
 import umap
 import sklearn.cluster as cluster
+from sklearn import decomposition
 
 DEFAULTS = {
-    "min_dist": 9.0,
+    "dimensionality_reduction_method": "umap",
+    "min_dist": 0.0,
     "n_neighbors": 27,
     "sample_size": 2500,
     "spread": 21,
@@ -37,21 +39,12 @@ sums = np.sum(data, axis=1)
 data /= sums[:, None]
 num_rows_to_select = 5000
 # Randomly select unique row indices
-random_indices = np.random.choice(
-    data.shape[0], num_rows_to_select, replace=False
-)
+random_indices = np.random.choice(data.shape[0], num_rows_to_select, replace=False)
 points = data[random_indices]
 U = None
 
 
-def fit(
-    min_dist, n_neighbors, spread, repulsion_strength, sample_size, dimension
-):
-    global points
-    random_indices = np.random.choice(
-        data.shape[0], sample_size, replace=False
-    )
-    points = data[random_indices]
+def umap_fit(points, min_dist, n_neighbors, spread, repulsion_strength, dimension):
     fit = umap.UMAP(
         min_dist=min_dist,
         repulsion_strength=repulsion_strength,
@@ -64,17 +57,42 @@ def fit(
     return u
 
 
+def pca_fit(points, dimension):
+    fit = decomposition.PCA(n_components=dimension)
+    u = fit.fit_transform(points)
+    print("Done")
+    return u
+
+
 server = get_server(client_type="vue3")
-state, ctrl = server.state, server.controller
+state = server.state
 state.trame__title = "umap explorer"
+
+
+def sample_data(data, sample_size):
+    global points
+    random_indices = np.random.choice(data.shape[0], sample_size, replace=False)
+    points = data[random_indices]
+
+
+def fit_data(points):
+    if state.dimensionality_reduction_method == "umap":
+        return umap_fit(
+            points,
+            state.min_dist,
+            state.n_neighbors,
+            state.spread,
+            state.repulsion_strength,
+            state.dimension,
+        )
+    elif state.dimensionality_reduction_method == "pca":
+        return pca_fit(points, state.dimension)
 
 
 @state.change("color_by")
 def on_color_by(color_by, **kwargs):
     if U is not None:
-        server.controller.figure_update(
-            scatter(U, color_by, state.clustering_method, state.dimension)
-        )
+        server.controller.figure_update(scatter(U, COLOR_ARGS, state.dimension))
 
 
 @state.change(
@@ -85,22 +103,21 @@ def on_color_by(color_by, **kwargs):
     "max_eps",
 )
 def on_clustering_method(**kwargs):
+    global COLOR_ARGS
     if U is not None:
-        server.controller.figure_update(
-            scatter(
-                U, state.color_by, state.clustering_method, state.dimension
-            )
+        COLOR_ARGS = get_color_args(
+            color_by=state.color_by, clustering_method=state.clustering_method
         )
+        server.controller.figure_update(scatter(U, COLOR_ARGS, state.dimension))
 
 
 @state.change("marker_size")
-def on_marker_size(**kwargs):
+def on_marker_size(marker_size, **kwargs):
+    global COLOR_ARGS
     if U is not None:
-        server.controller.figure_update(
-            scatter(
-                U, state.color_by, state.clustering_method, state.dimension
-            )
-        )
+        COLOR_ARGS["marker"]["size"] = marker_size
+
+        server.controller.figure_update(scatter(U, COLOR_ARGS, state.dimension))
 
 
 def get_color_args(color_by=None, clustering_method=None):
@@ -141,54 +158,51 @@ def get_color_args(color_by=None, clustering_method=None):
     return color_args
 
 
-def scatter(U, color_by=None, clustering_method=None, dimension=2):
-    color_args = get_color_args(
-        color_by=color_by, clustering_method=clustering_method
-    )
+def on_selection(selection_data):
+    print(selection_data)
+
+
+def scatter(U, color_args=None, dimension=2):
+    print(f"{points.shape}")
     if dimension == 2:
         plot = go.Scatter(
             x=U[:, 0],
             y=U[:, 1],
-            mode='markers',
+            mode="markers",
             **color_args,
             customdata=points,
-            hovertemplate='%{customdata[0]:.3f} <br> %{customdata[1]:.3f} <br> %{customdata[2]:.3f} <br> %{customdata[3]:.3f} ',
-            name='',
+            hovertemplate="%{customdata[0]:.3f} <br> %{customdata[1]:.3f} <br> %{customdata[2]:.3f} <br> %{customdata[3]:.3f} ",
+            name="",
         )
     else:
         plot = go.Scatter3d(
             x=U[:, 0],
             y=U[:, 1],
             z=U[:, 2],
-            mode='markers',
+            mode="markers",
             **color_args,
             customdata=points,
-            hovertemplate='%{customdata[0]:.3f} <br> %{customdata[1]:.3f} <br> %{customdata[2]:.3f} <br> %{customdata[3]:.3f} ',
-            name='',
+            hovertemplate="%{customdata[0]:.3f} <br> %{customdata[1]:.3f} <br> %{customdata[2]:.3f} <br> %{customdata[3]:.3f} ",
+            name="",
         )
-    return go.Figure(data=plot).update_layout(
-        margin=dict(l=10, r=10, t=25, b=10)
-    )
+    return go.Figure(data=plot).update_layout(margin=dict(l=10, r=10, t=25, b=10))
 
 
 # -----------------------------------------------------------------------------
 # Callbacks
 # -----------------------------------------------------------------------------
 def update_plot():
-    global U
-    U = fit(
-        state.min_dist,
-        state.n_neighbors,
-        state.spread,
-        state.repulsion_strength,
-        state.sample_size,
-        state.dimension,
+    global U, COLOR_ARGS
+    sample_data(data, state.sample_size)
+    U = fit_data(points)
+    COLOR_ARGS = get_color_args(
+        color_by=state.color_by, clustering_method=state.clustering_method
     )
+    print(f"{U.shape=}")
     server.controller.figure_update(
         scatter(
             U,
-            color_by=state.color_by,
-            clustering_method=state.clustering_method,
+            COLOR_ARGS,
             dimension=state.dimension,
         )
     )
@@ -204,8 +218,52 @@ with SinglePageWithDrawerLayout(server) as layout:
     layout.title.set_text("trame+plotly+umap")
 
     with layout.drawer:
-        with vuetify3.VCard(classes="mb-2 mx-1"):
+        vuetify3.VSlider(
+            label="sample_size",
+            v_model=("sample_size", DEFAULTS["sample_size"]),
+            min=100,
+            max=100000,  # data.shape[0],
+            step=100,
+            hide_details=True,
+            thumb_label=True,
+        )
+        vuetify3.VSelect(
+            label="method",
+            v_model=(
+                "dimensionality_reduction_method",
+                DEFAULTS["dimensionality_reduction_method"],
+            ),
+            items=(
+                "reduction_methods",
+                [
+                    {"title": "umap", "value": "umap"},
+                    {"title": "pca", "value": "pca"},
+                ],
+            ),
+        )
+        vuetify3.VBtn("Go", click=update_plot)
+        with vuetify3.VCard(
+            classes="mb-2 mx-1", v_show="dimensionality_reduction_method === 'pca'"
+        ):
             with vuetify3.VCardText():
+                vuetify3.VSelect(
+                    label="dimension",
+                    v_model=("dimension", 2),
+                    items=(
+                        "dimensions",
+                        [
+                            {"title": "2", "value": 2},
+                            {"title": "3", "value": 3},
+                        ],
+                    ),
+                )
+
+        with vuetify3.VCard(
+            classes="mb-2 mx-1", v_show="dimensionality_reduction_method === 'umap'"
+        ):
+            with vuetify3.VCardText():
+                vuetify3.VSpacer()
+                vuetify3.VSpacer()
                 vuetify3.VSpacer()
                 vuetify3.VSlider(
                     label="min_dist",
@@ -230,15 +288,6 @@ with SinglePageWithDrawerLayout(server) as layout:
                 vuetify3.VSpacer()
                 vuetify3.VSpacer()
                 vuetify3.VSlider(
-                    label="sample_size",
-                    v_model=("sample_size", DEFAULTS["sample_size"]),
-                    min=100,
-                    max=10000,  # data.shape[0],
-                    step=100,
-                    hide_details=True,
-                    thumb_label=True,
-                )
-                vuetify3.VSlider(
                     label="spread",
                     v_model=("spread", DEFAULTS["spread"]),
                     min=1,
@@ -259,127 +308,128 @@ with SinglePageWithDrawerLayout(server) as layout:
                     hide_details=True,
                     thumb_label=True,
                 )
-                vuetify3.VBtn("Go", click=update_plot)
-                vuetify3.VSelect(
-                    v_model=("color_by", None),
-                    items=(
-                        "channel",
-                        [
-                            {"title": "None", "value": None},
-                            {"title": "0", "value": 0},
-                            {"title": "1", "value": 1},
-                            {"title": "2", "value": 2},
-                            {"title": "3", "value": 3},
-                            {"title": "10", "value": 10},
-                            {"title": "20", "value": 20},
-                            {"title": "30", "value": 30},
-                            {"title": "12", "value": 12},
-                            {"title": "13", "value": 13},
-                            {"title": "23", "value": 23},
-                        ],
-                    ),
-                )
-                vuetify3.VSlider(
-                    label="marker size",
-                    v_model=(
-                        "marker_size",
-                        DEFAULTS["marker_size"],
-                    ),
-                    min=0.1,
-                    max=3,
-                    step=0.01,
-                    hide_details=True,
-                    thumb_label=True,
-                )
-                vuetify3.VSelect(
-                    label="dimension",
-                    v_model=("dimension", 2),
-                    items=(
-                        "dimensions",
-                        [
-                            {"title": "2", "value": 2},
-                            {"title": "3", "value": 3},
-                        ],
-                    ),
-                )
-                vuetify3.VSelect(
-                    label="clustering",
-                    v_model=(
-                        "clustering_method",
-                        DEFAULTS["clustering_method"],
-                    ),
-                    items=(
-                        "methods",
-                        [
-                            {"title": "None", "value": None},
-                            {"title": "kmeans", "value": "kmeans"},
-                            {"title": "hdbscan", "value": "hdbscan"},
-                            {"title": "optics", "value": "optics"},
-                        ],
-                    ),
-                )
-                # kmeans panel
-                with html.Div(
-                    v_show="clustering_method === 'kmeans'",
-                ):
-                    vuetify3.VSpacer()
-                    vuetify3.VSlider(
-                        label="n_clusters",
-                        v_model=("n_clusters", DEFAULTS["n_clusters"]),
-                        min=1,
-                        max=100,
-                        step=1,
-                        hide_details=True,
-                        thumb_label=True,
-                    )
-                with html.Div(
-                    v_show="clustering_method === 'hdbscan'",
-                ):
-                    vuetify3.VSpacer()
-                    vuetify3.VSlider(
-                        label="min_samples",
-                        v_model=("min_samples", DEFAULTS["min_samples"]),
-                        min=1,
-                        max=1000,
-                        step=1,
-                        hide_details=True,
-                        thumb_label=True,
-                    )
-                    vuetify3.VSlider(
-                        label="min_cluster_size",
-                        v_model=(
-                            "min_cluster_size",
-                            DEFAULTS["min_cluster_size"],
-                        ),
-                        min=1,
-                        max=1000,
-                        step=1,
-                        hide_details=True,
-                        thumb_label=True,
-                    )
-                # kmeans panel
-                with html.Div(
-                    v_show="clustering_method === 'optics'",
-                ):
-                    vuetify3.VSpacer()
-                    vuetify3.VSlider(
-                        label="min_samples",
-                        v_model=("min_samples", DEFAULTS["min_samples"]),
-                        min=1,
-                        max=100,
-                        step=1,
-                        hide_details=True,
-                        thumb_label=True,
-                    )
-                    vuetify3.VSlider(
-                        label="max_eps",
-                        v_model=("max_eps", DEFAULTS["max_eps"]),
-                        min=1,
-                        max=200,
-                        step=1,
-                        hide_details=True,
-                        thumb_label=True,
-                    )
+
+        vuetify3.VSelect(
+            v_model=("color_by", None),
+            items=(
+                "channel",
+                [
+                    {"title": "None", "value": None},
+                    {"title": "0", "value": 0},
+                    {"title": "1", "value": 1},
+                    {"title": "2", "value": 2},
+                    {"title": "3", "value": 3},
+                    {"title": "10", "value": 10},
+                    {"title": "20", "value": 20},
+                    {"title": "30", "value": 30},
+                    {"title": "12", "value": 12},
+                    {"title": "13", "value": 13},
+                    {"title": "23", "value": 23},
+                ],
+            ),
+        )
+        vuetify3.VSlider(
+            label="marker size",
+            v_model=(
+                "marker_size",
+                DEFAULTS["marker_size"],
+            ),
+            min=0.1,
+            max=3,
+            step=0.01,
+            hide_details=True,
+            thumb_label=True,
+        )
+        vuetify3.VSelect(
+            label="dimension",
+            v_model=("dimension", 2),
+            items=(
+                "dimensions",
+                [
+                    {"title": "2", "value": 2},
+                    {"title": "3", "value": 3},
+                ],
+            ),
+        )
+
+        vuetify3.VSelect(
+            label="clustering method",
+            v_model=(
+                "clustering_method",
+                DEFAULTS["clustering_method"],
+            ),
+            items=(
+                "clustering_methods",
+                [
+                    {"title": "None", "value": None},
+                    {"title": "kmeans", "value": "kmeans"},
+                    {"title": "hdbscan", "value": "hdbscan"},
+                    {"title": "optics", "value": "optics"},
+                ],
+            ),
+        )
+        # kmeans panel
+        with html.Div(
+            v_show="clustering_method === 'kmeans'",
+        ):
+            vuetify3.VSpacer()
+            vuetify3.VSlider(
+                label="n_clusters",
+                v_model=("n_clusters", DEFAULTS["n_clusters"]),
+                min=1,
+                max=100,
+                step=1,
+                hide_details=True,
+                thumb_label=True,
+            )
+        with html.Div(
+            v_show="clustering_method === 'hdbscan'",
+        ):
+            vuetify3.VSpacer()
+            vuetify3.VSlider(
+                label="min_samples",
+                v_model=("min_samples", DEFAULTS["min_samples"]),
+                min=1,
+                max=1000,
+                step=1,
+                hide_details=True,
+                thumb_label=True,
+            )
+            vuetify3.VSlider(
+                label="min_cluster_size",
+                v_model=(
+                    "min_cluster_size",
+                    DEFAULTS["min_cluster_size"],
+                ),
+                min=1,
+                max=1000,
+                step=1,
+                hide_details=True,
+                thumb_label=True,
+            )
+        # optics panel
+        with html.Div(
+            v_show="clustering_method === 'optics'",
+        ):
+            vuetify3.VSpacer()
+            vuetify3.VSlider(
+                label="min_samples",
+                v_model=("min_samples", DEFAULTS["min_samples"]),
+                min=1,
+                max=100,
+                step=1,
+                hide_details=True,
+                thumb_label=True,
+            )
+            vuetify3.VSlider(
+                label="max_eps",
+                v_model=("max_eps", DEFAULTS["max_eps"]),
+                min=1,
+                max=200,
+                step=1,
+                hide_details=True,
+                thumb_label=True,
+            )
 
         with vuetify3.VCard(classes="mb-2 mx-1"):
             with vuetify3.VCardText():
@@ -396,10 +446,9 @@ with SinglePageWithDrawerLayout(server) as layout:
                 figure = plotly.Figure(
                     display_logo=False,
                     display_mode_bar="true",
+                    # selected = (on_selection, "[ 'selected', utils.safe($event)"]),
                 )
                 server.controller.figure_update = figure.update
-                ctrl.figure_update = figure.update
-                vuetify3.VSpacer()
 
 # -----------------------------------------------------------------------------
 # Main
